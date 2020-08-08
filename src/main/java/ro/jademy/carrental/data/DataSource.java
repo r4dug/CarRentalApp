067d744f93f556885a06ab919d4bc9b8d53195f1
Radu Guran <radug314@gmail.com>
2020-08-08 21:35:19 +0300

    Added CAR_LIST in DataSource class.

--- src/main/java/ro/jademy/carrental/data/DataSource.java
+++ src/main/java/ro/jademy/carrental/data/DataSource.java
@@ -1,12 +1,20 @@
 package ro.jademy.carrental.data;
 
 import ro.jademy.carrental.model.Car;
+import ro.jademy.carrental.model.cars.Toyota;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DataSource {
 
     // create a list with the users
+
     // create a list with the cars
+    public static final List<Car> CAR_LIST = Arrays.asList(
+            new Toyota("Toyota", "C-HR", "Hybrid", "SUV", "Automatic", 123456, 5, 4, 100)
+
+    );
 
 }
--- src/main/java/ro/jademy/carrental/model/cars/Toyota.java
+++ src/main/java/ro/jademy/carrental/model/cars/Toyota.java
@@ -5,12 +5,7 @@ import ro.jademy.carrental.model.Car;
 
 public class Toyota extends Car {
 
-    public Toyota () {}
-
-    Car car1 = new Car("Toyota", "C-HR", "Hybrid", "SUV", "Automatic", 123456, 5, 4, 100);
-
-
-    public void getCarSpecs () {
-        System.out.println("ID:"+ car1.getCarId() + " Make:" + car1.getMake() + " Model:" + car1.getModel() + " FuelType:" + car1.getFuelType());
+    public Toyota(String make, String model, String fuelType, String carType, String transmissionType, int carId, int nrOfSeats, int nrOfDoors, double pricePerDay) {
+        super(make, model, fuelType, carType, transmissionType, carId, nrOfSeats, nrOfDoors, pricePerDay);
     }
 }
